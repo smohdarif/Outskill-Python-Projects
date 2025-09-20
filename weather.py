ccb252c167585c1b9c47a144db95c9c00065f0f8
Arif Shaikh <smohdarif@gmail.com>
2025-09-20 12:47:43 -0400

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "https://open-weather13.p.rapidapi.com/city"
+
+querystring = {"city":"Atlanta","lang":"EN"}
+
+headers = {
+	"x-rapidapi-key": "b21697a4f3mshc18a3118f1a2e72p142c1bjsn2433e369a8a8",
+	"x-rapidapi-host": "open-weather13.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+print(response.json())
+
+print(json.dumps(response.json(),indent=4))
